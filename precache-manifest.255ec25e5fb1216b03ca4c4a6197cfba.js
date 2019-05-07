336fc53d8b61acbc81ab576a7fd1cf93750ebbd9
adel <adel.mohamed855@gmail.com>
2019-05-07 22:54:42 +0200

    Updates

---
+++ precache-manifest.255ec25e5fb1216b03ca4c4a6197cfba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af936da01470f187647d0a59c923aa7",
+    "url": "/roadtrex/index.html"
+  },
+  {
+    "revision": "f57787cb1433bac96055",
+    "url": "/roadtrex/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/roadtrex/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f57787cb1433bac96055",
+    "url": "/roadtrex/static/js/main.a4e3b3c8.chunk.js"
+  },
+  {
+    "revision": "996c7533b6422121ebcc",
+    "url": "/roadtrex/static/js/runtime~main.af74900d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roadtrex/static/media/logo.5d5d9eef.svg"
+  }
+]);
